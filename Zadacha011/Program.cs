dc7cb750abf02a0019e816724b32f5e7e067320c
Maria <vilochka-17@yandex.ru>
2022-11-06 19:48:52 +0300

    Zadacha11 made

---
+++ Zadacha011/Program.cs
@@ -0,0 +1,7 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next(10, 99);
+Console.WriteLine(number);
+int num1 = number / 10;
+int num2 = number % 10;
+if(num1>num2) Console.WriteLine($"Первая цифра {num1} больше, чем вторая {num2}");
+else Console.WriteLine($"Вторая цифра {num2} больше, чем первая {num1}");
