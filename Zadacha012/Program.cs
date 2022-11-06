9bdc75b0ca1d611eed0365357a982c8c5ff1d0b0
Maria <vilochka-17@yandex.ru>
2022-11-06 20:24:11 +0300

    Zadacha12 made

---
+++ Zadacha012/Program.cs
@@ -0,0 +1,15 @@
+﻿// Удалить вторую цифру трёхзначного числа
+// Console.WriteLine("Введите трехзначаное число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// while (num < 100 | num >= 1000)
+// {
+//     Console.WriteLine("Ошибка.Введите трехзначаное число:");
+//     num = Convert.ToInt32(Console.ReadLine());
+// }
+
+// int NewNum = (num / 100) * 10 + num % 10;
+// Console.WriteLine($"При удалении второго числа,получается число {NewNum}");
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int NewNum = (num / 100) * 10 + num % 10;
+Console.WriteLine($"При удалении второго числа,получается число {NewNum}");
