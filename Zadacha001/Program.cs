a740e6a1ef328be43435d0f617e69fd596716805
Maria <vilochka-17@yandex.ru>
2022-11-06 11:39:03 +0300

    Zadacha1 made

---
+++ Zadacha001/Program.cs
@@ -0,0 +1,8 @@
+﻿// 1. По двум заданным числам проверять 
+// является ли первое квадратом второго
+Console.WriteLine("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+double Squ = Math.Sqrt(number);
+Console.WriteLine($"Квадрат числа {number} = {Squ}");
+if (Squ % 2 == 0) Console.WriteLine("Да, первое число является квадратом второго");
+else Console.WriteLine("Нет, первое число не является квадратом второго");
