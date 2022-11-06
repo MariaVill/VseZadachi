2ebfd61a6ddb9796b1b1ac95abbd1ebea4a3fbc9
Maria <vilochka-17@yandex.ru>
2022-11-06 10:58:27 +0300

    Zadacha0 made

---
+++ Zadacha000/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вывести квадрат числа
+Console.WriteLine("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+double Pow = Math.Pow(number, 2);
+Console.WriteLine ($"Квадрат числа {number} равен {Pow}");
