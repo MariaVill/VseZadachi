8fc8ffbd83d285205966f587add3195be09b29a3
Maria <vilochka-17@yandex.ru>
2022-11-06 15:45:01 +0300

    Zadacha006 made

---
+++ Zadacha006/Program.cs
@@ -0,0 +1,6 @@
+﻿// 6. Выяснить является ли число чётным
+// если есть логическое выражение,то используют if. если есть шаблон, то switch
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0) Console.WriteLine("Да. Число является четным");
+else Console.WriteLine("Нет. Число не является четным");
