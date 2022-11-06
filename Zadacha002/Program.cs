9bbad8b183bf266bc6c6c2e818657cf35587b10f
Maria <vilochka-17@yandex.ru>
2022-11-06 11:56:54 +0300

    Zadacha2 made

---
+++ Zadacha002/Program.cs
@@ -0,0 +1,7 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите два числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB) Console.WriteLine($"Число {numberA} больше, чем {numberB}");
+else Console.WriteLine($"Число {numberA} меньше, чем {numberB}");
