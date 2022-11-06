39cf4236d2632d126b99d34264674f6c8dc7e7a3
Maria <vilochka-17@yandex.ru>
2022-11-06 19:55:39 +0300

    Zadacha8 made

---
+++ Zadacha008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i < num)
+{
+    Console.WriteLine(i);
+    i = i + 2;
+}
+
