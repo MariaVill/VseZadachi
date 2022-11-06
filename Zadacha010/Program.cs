6930fb3d46845ce734197734b01d09ffc950c28e
Maria <vilochka-17@yandex.ru>
2022-11-06 16:40:36 +0300

    Zadacha10 made

---
+++ Zadacha010/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < 100 | num >= 1000)
+{
+    Console.WriteLine("Ошибка. Введите трехзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+int result = (num / 10) % 10;
+Console.WriteLine($"Вторая цифра у числа {num} это {result}");
