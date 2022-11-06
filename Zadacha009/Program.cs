0a2af9fd5f4a2884796274133e12c294e9460c83
Maria <vilochka-17@yandex.ru>
2022-11-06 16:33:02 +0300

    Zadacha9 made

---
+++ Zadacha009/Program.cs
@@ -0,0 +1,10 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while (num < 100 | num >= 1000)
+{
+    Console.WriteLine("Ошибка. Введите трехзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+int result = num % 10;
+Console.WriteLine($"Третья цифра у числа {num} это {result}");
