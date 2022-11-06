7a777ab57753187bfb4574336758cf99705d3731
Maria <vilochka-17@yandex.ru>
2022-11-06 16:22:20 +0300

    Zadacha7 made

---
+++ Zadacha007/Program.cs
@@ -0,0 +1,10 @@
+﻿// 7. Показать числа от -N до N
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = -num1;
+Console.WriteLine($"Список чисел от {num2} до {num1}");
+while (num2 <= num1)
+{
+    Console.WriteLine(num2++);
+}
