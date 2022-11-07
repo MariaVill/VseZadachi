840a97736d950fa76cc473ea2b0546a6cc5dc86b
Maria <vilochka-17@yandex.ru>
2022-11-07 10:53:36 +0300

    Zadacha013, Zadacha014 made

---
+++ Zadacha013/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.mod%
+int num1 = new Random().Next(50, 100);
+int num2 = new Random().Next(1, 49);
+
+Console.WriteLine($"Первое число {num1} и второе число {num2}");
+if (num1 % num2 == 0)
+{
+    Console.WriteLine($"Число {num1} кратно {num2}");
+}
+else
+{
+   Console.WriteLine($"Число {num1} некратно {num2}, остаток от деления равен {num1 % num2}");
+}
+
+
---
+++ Zadacha014/Program.cs
@@ -0,0 +1,15 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+int num = 76245;
+int num3 = 0;                        //new Random().Next(1, 10000);
+// Console.WriteLine(num);
+if (num >= 100)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    num3 = num % 10;
+    Console.WriteLine($"Третья цифра данного числа это {num3}");
+}
+else Console.WriteLine($"У данного числа тетьей цифры нет");
+
